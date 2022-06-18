ee826006ac62d9f696fb4eb03444a1b070fb5db2
Nishant <nishants440@gmail.com>
2022-06-18 18:04:54 +0530

    AutoForms React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('')
+  const [address, setAddress] = useState('');
+  const ALAN_Key = `Your ALAN AI KEY`
+  useEffect(() => {
+    alanBtn({
+        key: ALAN_Key,
+        onCommand: (commandData) => {
+          if(commandData.command === 'name'){
+            setName(commandData.data)
+          }
+          if(commandData.command === 'phone'){
+            setPhone(commandData.data)
+          }
+          if(commandData.command === 'address'){
+            setAddress(commandData.data)
+          }
+        }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form-fields'>
+      <input
+        placeholder='Name'
+        name='name'
+        onChange={(event) => setName(event.target.value)}
+        value={name}
+        type='text'
+      />
+      <input
+        placeholder='Phone'
+        name='phone'
+        onChange={(event) => setPhone(event.target.value)}
+        value={phone}
+        type='text'
+      />
+      <input
+        placeholder='Address'
+        name='address'
+        onChange={(event) => setAddress(event.target.value)}
+        value={address}
+        type='text'
+      />
+
+      <button className='submit-btn'>Submit</button>
     </div>
   );
 }
